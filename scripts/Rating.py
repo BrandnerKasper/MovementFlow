7d8895d809e7d0690a1df1d1785d92ff0a3f7b28
Tobi <tobias.brandner@stud-mail.uni-wuerzburg.de>
2023-08-03 15:18:04 +0200

    updated rating png

--- scripts/Rating.py
+++ scripts/Rating.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 games = ['Jump', 'Game Flow', 'Difficulty']
 BnR_values = [4.45, 4, 3.09]
 SM64_values = [3.16, 3.08, 4.16]
-DK64_values = [3.5, 3.5, 3.25]
 BK_values = [4.42, 4.42, 2.71]
+DK64_values = [3.5, 3.5, 3.25]
 
 # Set the width of the bars
 bar_width = 0.15
@@ -14,16 +14,16 @@ bar_width = 0.15
 x_positions = range(len(games))
 
 # Create the bar chart for BnR game values
-plt.bar(x_positions, BnR_values, width=bar_width, label='BnR', alpha=0.8, edgecolor='black')
+plt.bar(x_positions, BnR_values, width=bar_width, label='Boss\'n Run', alpha=0.7, edgecolor='black')
 
 # Create the bar chart for SM64 game values (shifted by bar_width)
-plt.bar([pos + bar_width for pos in x_positions], SM64_values, width=bar_width, label='SM64', alpha=0.8, edgecolor='black')
+plt.bar([pos + bar_width for pos in x_positions], SM64_values, width=bar_width, label='Super Mario 64', alpha=0.7, edgecolor='black')
 
 # Create the bar chart for DK64 game values (shifted by 2 * bar_width)
-plt.bar([pos + 2 * bar_width for pos in x_positions], DK64_values, width=bar_width, label='DK64', alpha=0.8, edgecolor='black')
+plt.bar([pos + 2 * bar_width for pos in x_positions], BK_values, width=bar_width, label='Banjo Kazooie', alpha=0.7, edgecolor='black')
 
 # Create the bar chart for BK game values (shifted by 3 * bar_width)
-plt.bar([pos + 3 * bar_width for pos in x_positions], BK_values, width=bar_width, label='BK', alpha=0.8, edgecolor='black')
+plt.bar([pos + 3 * bar_width for pos in x_positions], DK64_values, width=bar_width, label='Donkey Kong 64', alpha=0.7, edgecolor='black')
 
 # Set the x-axis tick positions and labels
 plt.xticks([pos + 1.5 * bar_width for pos in x_positions], games, fontsize=12)
